e4a410ab3d35363713eff47596cd3ece8482c333
ygong1 <ygong1>
2023-10-06 21:09:32 +0000

    convert to notbook

--- metrics-analysis/workloads.py
+++ metrics-analysis/workloads.py
@@ -1,4 +1,12 @@
-# Databricks 
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC ## README
+# MAGIC 
+# MAGIC This script goes through the `prod.workload_insights` table, filters the AutoML related entries,
+# MAGIC and writes them into a separate `ml.automl_workflow_insights` table.
+# MAGIC The result table is much smaller (26,874 entries as of 4/18/2022) than the original dataset
+# MAGIC and more suitable for interactive analysis and dashboard (e.g. go/automl/usage).
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 
