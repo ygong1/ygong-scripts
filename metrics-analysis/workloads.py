e03a2a4d1690a31a4ff9c53993e9e2577d85bbaf
ygong1 <ygong1>
2023-10-08 20:58:53 +0000

    updated for metrics analysis

--- metrics-analysis/workloads.py
+++ metrics-analysis/workloads.py
@@ -1,28 +1,62 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC 
+# MAGIC
 # MAGIC ## README
-# MAGIC 
+# MAGIC
 # MAGIC This script goes through the `prod.workload_insights` table, filters the AutoML related entries,
 # MAGIC and writes them into a separate `ml.automl_workflow_insights` table.
 # MAGIC The result table is much smaller (26,874 entries as of 4/18/2022) than the original dataset
 # MAGIC and more suitable for interactive analysis and dashboard (e.g. go/automl/usage).
-from pyspark.sql import SparkSession
-from pyspark.sql.types import *
+# MAGIC
 
-spark = SparkSession.builder.getOrCreate()
+# COMMAND ----------
 
-schema = StructType([
-   StructField('CustomerID', IntegerType(), False),
-   StructField('FirstName',  StringType(),  False),
-   StructField('LastName',   StringType(),  False)
-])
+# MAGIC %sql
+# MAGIC CREATE TABLE users.yu_gong.workload_insights_10days SELECT * from prod.workload_insights where customerType IN ("Customer", "MicrosoftPaid") AND canonicalCustomerName NOT IN ("Databricks", "Microsoft") AND date < date_trunc('week', current_date()) AND date >= DATEADD(day, -10, current_date())
 
-data = [
-   [ 1000, 'Mathijs', 'Oosterhout-Rijntjes' ],
-   [ 1001, 'Joost',   'van Brunswijk' ],
-   [ 1002, 'Stan',    'Bokenkamp' ]
-]
+# COMMAND ----------
 
-customers = spark.createDataFrame(data, schema)
-customers.show()
+from pyspark.sql.functions import col
+df = spark.sql(f''' SELECT * from  users.yu_gong.workload_insights_10days''').select ('date', 'cloudType', 'canonicalCustomerName', 'workloadType', 'workloadName', 'workloadFeatureFlags', 'clusterTags', 'clusterFeatureFlags', 'attributedInfo', 'attributedRevenue', 'packages').filter(col("workloadFeatureFlags").isNotNull())
+df.cache()
+
+# COMMAND ----------
+
+from pyspark.sql.functions import udf
+from typing import Dict, List
+from pyspark.sql.types import ArrayType, StringType
+
+# ML, horovod, horovodrunner, petastorm, pytorch, sklearn
+# sparkdl, tensorflow, xgboost
+@udf(returnType=ArrayType(StringType()))
+def extractWorkload(workloadFeatureFlags: Dict[str, str])-> List[str]:
+  workloadTypes = []
+  if workloadFeatureFlags != None:
+    for k in workloadFeatureFlags.keys():
+      if workloadFeatureFlags[k]:
+        workloadTypes.append(k)
+  return workloadTypes
+
+# COMMAND ----------
+
+Find out all the different 
+
+# COMMAND ----------
+
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import lit
+
+allWorkloads = df \
+  .withColumn("MLworkloads", extractWorkload(col("workloadFeatureFlags"))) \
+  .select(explode("MLworkloads").alias("ygong")).distinct().collect()
+  
+# values = tmp.collect()
+
+# for r in values:
+#   print(r)
+  
+
+# COMMAND ----------
+
+tmp.select("ygong").distinct().show()
+
